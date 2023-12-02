5bccf1605adf67b76ffd3c89b9439eb55c2070eb
odesai840 <odesai840@gmail.com>
2023-12-02 16:53:08 -0500

    fixed issues with cors headers, but godot games still have issues with loading

--- app.py
+++ app.py
@@ -1,5 +1,4 @@
 from flask import Flask, render_template, request, redirect, url_for, session, jsonify
-from flask_cors import CORS
 from werkzeug.utils import secure_filename
 import zipfile
 import shutil
@@ -52,13 +51,12 @@ ALLOWED_COVER_EXTENSIONS = {'png', 'jpg', 'jpeg', 'gif'}
 # Allowed game file extensions
 ALLOWED_GAME_EXTENSIONS = {'zip'}
 
-#@app.after_request
-#def add_cors_headers(response):
-#    response.headers['Cross-Origin-Embedder-Policy'] = 'require-corp'
-#    response.headers['Cross-Origin-Opener-Policy'] = 'same-origin'
-#    response.headers['Cross-Origin-Resource-Policy'] = 'cross-origin'
-#
-#    return response
+@app.after_request
+def add_cors_headers(response):
+   response.headers['Cross-Origin-Embedder-Policy'] = 'credentialless'
+   response.headers['Cross-Origin-Opener-Policy'] = 'same-origin'
+
+   return response
 
 def get_game_details_from_rawg_api(game_id):
     API_KEY = os.getenv('API_KEY')
